fa66e54a1ff5f5ab612ca2466a9eac35b991614e
jimJunhyun <tsumego2015@gmail.com>
2023-05-31 14:42:30 +0900

    fence and map
    structure basic

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    protected Transform target;
+    public int hp;
+    public int atk;
+    public float speed;
+
+
+
+
+
+    
+}
